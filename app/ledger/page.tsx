08b39bf3204712d1e85b550341096b1e599777c4
raileats <raileats@gmail.com>
2026-07-17 14:17:07 +0530

    Update page.tsx

--- app/ledger/page.tsx
+++ app/ledger/page.tsx
@@ -7,13 +7,4 @@ export const revalidate = 0;
 
 export default function LedgerPage() {
   return <LedgerClient />;
-}// app/ledger/page.tsx
-
-import LedgerClient from "@/components/restro/LedgerClient";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function LedgerPage() {
-  return <LedgerClient />;
 }
